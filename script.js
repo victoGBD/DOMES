181f160b3c7a4240935c59f07c3c692e0964b8df
victoGBD <101598672+victoGBD@users.noreply.github.com>
2024-05-17 09:06:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
+//Funcoes que tratam de acoes nos botoes 
+
+
+function transicao(){
+    var transitionScreen = document.querySelector(".transition-screen");
+    transitionScreen.classList.add("hidden");
+    setTimeout(function() {
+        transitionScreen.remove();
+    }, 1000);
+    window.location.href = "index.html";
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-      var transitionScreen = document.querySelector(".transition-screen");
-
-      transitionScreen.classList.add("hidden");
-
-      setTimeout(function() {
-          transitionScreen.remove();
-      }, 1000);
-      window.location.href = "index.html";
-  }, 3000); // 3 segundos em milissegundos
-    // Função para redirecionar para a página de menu
+  setTimeout(transicao , 3000); // 3s (em milisegundos)
   });
 
 document.addEventListener("DOMContentLoaded", function() {
-    function redirectToMenu(event) {
-      event.preventDefault(); // Previne o envio padrão do formulário
-      window.location.href = 'menu.html'; // Redireciona para a página menu.html
-  }
-
-  // Adiciona o evento de submissão ao formulário
-  document.getElementById('login-form').addEventListener('submit', redirectToMenu);
-  // Adiciona funcionalidade ao ícone de menu
-  const menuIcon = document.getElementById('menu-icon');
-  const dropdownMenu = document.getElementById('dropdown-menu');
-
-  menuIcon.addEventListener('click', function() {
-      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-  });
+    
 });
+  
+  document.getElementById('login-form').addEventListener('submit', redirectToMenu);
+  //faz o menu funcionar
+
+function controlarMenu(){
+  const dropdownMenu = document.getElementById('dropdown-menu');
+  dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+
+function adcionaEventoClickMenu(){
+  const menuIcon = document.getElementById('menu-icon');
+  menuIcon.addEventListener('click', function() {
+   controlarMenu(); 
+ });
+
+function redirectToMenu(event) {
+  event.preventDefault(); 
+  window.location.href = 'menu.html'; //Manda pro menu
+}
+
+}
+//todos os eventos de acao de click ficaram aqui
+adcionaEventoClickMenu();
+
+ 
 
